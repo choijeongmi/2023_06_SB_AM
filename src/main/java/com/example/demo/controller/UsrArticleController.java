658e8bb5509ba23a6624874f53cab082e1cbfe00
choijeongmi <checheda5@gmail.com>
2023-07-12 19:53:54 +0900

    writeArticle

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -20,6 +20,22 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for(int i = 1; i <=10; i++) {
+			
+			int id = this.lastArticleId + 1;
+			this.lastArticleId = id;
+			
+			String title = "제목" + i;
+			String body = "내용" + i; 
+			
+			articles.add(new Article(id, title, body));
+		}
+		
 	}
 
 	@RequestMapping("/usr/article/doAdd")
