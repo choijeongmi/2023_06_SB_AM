8148adf99f403497948dde1a5ef94a795328573b
choijeongmi <checheda5@gmail.com>
2023-07-14 20:34:58 +0900

    article  기본 기능 추가 (수정, 상세, 삭제)/ service,Dao 생성

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,62 +1,85 @@
 package com.example.demo.controller;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.vo.Article; 
+import com.example.demo.service.ArticleService;
+import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-
-	private int lastArticleId;
-	private List<Article> articles;
-
-	public UsrArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		
-		makeTestData();
+	
+	private ArticleService articleService;
+// @Autowired > 해당 변수 및 메서드에 스프링이 관리하는 Bean을 자동으로 매핑해주는 개념 	
+//	@Autowired > 필드주입                                           	    @Autowired >생성자 주입
+//	private ArticleService articleService;                                  public UsrArticleController(ArticleService articleService) {
+//                                                                              this.articleService = articleService;  }
+	@Autowired
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
-
-	private void makeTestData() {
-		for(int i = 1; i <=10; i++) {
-			
-			int id = this.lastArticleId + 1;
-			this.lastArticleId = id;
-			
-			String title = "제목" + i;
-			String body = "내용" + i; 
-			
-			articles.add(new Article(id, title, body));
-		}
-		
-	}
-
+	
+	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-
-		int id = this.lastArticleId + 1;
-		this.lastArticleId = id;
-
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-
+		
+		Article article = articleService.writeArticle(title, body);
+		
 		return article;
 	}
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
+		return articleService.getArticles();
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
 		
+		Article foundArticle = articleService.getArticleById(id);
 		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
 		
-		return this.articles;
+		return foundArticle;
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		articleService.modifyArticle(id, title, body);
+		
+		return id + "번 게시글을 수정했습니다";
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		articleService.deleteArticle(id);
+		
+		return id + "번 게시글을 삭제했습니다";
 	}
 }
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/*
+@AllArgsConstructor > 여기에 필드에 쓴 모든생성자만 만들어줌
+@NoArgsConstructor > 기본 생성자를 만들어줌
+@Data > getter, setter 만들어줌
+ */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
